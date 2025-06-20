fdb2a3c46ee062d68665ced534b8c985cba9c2cd
lucasnmc12 <lucas.nogueira20mc@gmail.com>
2025-06-20 10:55:12 -0300

    commit

--- plf-es-2023-2-ti3-6653100-msi-construtora-master/Codigo/frontend/scripts/editarCliente.js
+++
@@ -1,8 +0,0 @@
-
-//redirecionar para central do cliente
-const centralButton = document.getElementById("centralButton");
-
-// Adiciona um evento de clique ao botão
-centralButton.addEventListener("click", function () {
-  window.location.href = "/paginaCliente";
-});
